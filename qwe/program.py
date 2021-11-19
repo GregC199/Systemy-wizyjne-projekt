30ab9c35609cd171a00c42657e7214c06d794a9f
MichalB0505 <BIRNBACH.MICHAL@GMAIL.COM>
2021-11-19 16:27:07 +0100

    Utworzenie projektu

---
+++ qwe/program.py
@@ -0,0 +1,13 @@
+'''
+Created on 19 lis 2021
+
+@author: greg
+'''
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    
+    print('dupa')
+    pass
